a03b0d40f6e454a187d73d63faf2f2d460a95032
Eduardo Tavares <eduardo.tavares@faculdadenovaroma.com.br>
2024-05-14 19:25:15 -0300

    Criacao dos templates

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require("express");
 
 const app = express();
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World Web Server! </h1>").status(200);
+app.set("view engine", "ejs");
+app.set("views", [
+  "pages",
+]);
+
+app.get("/contato", (req, res) => {
+  res.render("contato");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/users", (req, res) => {
+  res.render("usuarios/lista");
+});
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello World Web Server! </h1>").status(200);
+// });
+
 app.listen(80, () => {
-    console.log("Server is running on port 80");
+  console.log("Server is running on port http://localhost:80");
 });
 
 
