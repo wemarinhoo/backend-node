f7cf29d92088c9bb7195ae6db9b2ebbde76d2d90
Wytheny <wtmarinho55@gmail.com>
2024-06-10 20:14:54 -0300

    refactor: refatoração da estrutura de código do controller

---
+++ app/controllers/AgendasController.js
@@ -0,0 +1,48 @@
+const Agenda = require("../models/Task");
+const agendasRepository = require("../repositories/AgendasRepository");
+
+function AgendaController() {
+  async function list(req, res) {
+    const agendas = await agendasRepository.list();
+
+    res.render("agendas/list", {
+      title: "Lista de Agendas",
+      agendas: agendas,
+    });
+  }
+
+  function create(req, res) {
+    res.render("agendas/create");
+  }
+
+  async function save(req, res) {
+    await agendasRepository.save(req.body);
+    res.redirect("/agendas");
+  }
+
+  async function remove(req, res) {
+    await agendasRepository.remove(req.params.id);
+    res.redirect("/agendas");
+  }
+
+  async function edit(req, res) {
+    const agenda = await agendasRepository.find(req.params.id);
+    res.render("agendas/edit", { agenda: agenda });
+  }
+
+  async function update(req, res) {
+    await agendasRepository.update(req.body.id, req.body);
+    res.redirect("/agendas");
+  }
+
+  return {
+    create,
+    save,
+    list,
+    remove,
+    edit,
+    update,
+  };
+}
+
+module.exports = AgendaController();
