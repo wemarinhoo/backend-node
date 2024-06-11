ffd394e54cf08433c163f93102867436fdcba612
Gabriel Oliveira Marinho <132677299+GabrielllOM@users.noreply.github.com>
2024-06-10 22:40:51 -0300

    feat: feito as rotas de api para as agendas

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const TasksController = require("../app/controllers/api/TasksController");
 const UsersController = require("../app/controllers/api/UsersController");
+const AgendasController = require("../app/controllers/api/AgendasController");
 
 // Rotas das Tarefas
 router.get('/tasks', TasksController.list)
@@ -11,6 +12,13 @@ router.post('/tasks', TasksController.save)
 router.delete('/tasks/:id', TasksController.remove)
 router.put('/tasks/:id', TasksController.update)
 router.put('/tasks/:id/update-status', TasksController.updateStatus)
+
+// Rotas das Tarefas
+router.get('/agendas', AgendasController.list)
+router.get('/agendas/:id', AgendasController.show)
+router.post('/agendas', AgendasController.save)
+router.delete('/agendas/:id', AgendasController.remove)
+router.put('/agendas/:id', AgendasController.update)
 
 
 router.get('/users', UsersController.list)
